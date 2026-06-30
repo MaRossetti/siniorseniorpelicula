bea0efa4267842853a274b4b7eeb97287b1f7776
Marco Rossetti <info@marcorossetti.es>
2026-06-30 13:17:16 +0200

    added audios
    Added audios

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,32 @@ function App() {
   const audioList = {
     rapidosenior: `/rapidosenior.ogg`,
     tenemosqueirnos: `/tenemosqueirnos.ogg`,
+    alcamnpoyacorrer: `/alcamnpoyacorrer.ogg`,
+    corredinsensatos: `/corredinsensatos.ogg`,
+    dios: `/dios.ogg`,
+    diosdiosdios: `/diosdiosdios.ogg`,
+    eltiodelosmelindros: `/eltiodelosmelindros.ogg`,
+    hayonohay : `/hayonohay.ogg`,
+    lapulicia: `/lapulicia.ogg`,
+    nopuedespasar: `/nopuedespasar.ogg`,
+    nosrobanlaspatatas: `/nosrobanlaspatatas.ogg`,
+    notocarloscojones: `/notocarloscojones.ogg`,
+    peliculagrande: `/peliculagrande.ogg`,
+    peliculagrandeopequena: `/peliculagrandeopequena.ogg`,
+    porfavorsinior: `/porfavorsinior.ogg`,
+    porfavorsoymuypobre: `/porfavorsoymuypobre.ogg`,
+    prohibidotocarloscojones: `/prohibidotocarloscojones.ogg`,
+    pulicia: `/pulicia.ogg`,
+    rapidoporfavor: `/rapidoporfavor.ogg`,
+    rapidoporfavorv2: `/rapidoporfavorv2.ogg`,
+    senioor: `/senioor.ogg`,
+    seniorespobre: `/seniorespobre.ogg`,
+    seniorespobrev2: `/seniorespobrev2.ogg`,
+    siniorseniorpelicula: `/siniorseniorpelicula.ogg`,
+    tenemoskirnos: `/tenemoskirnos.ogg`,
+    tenemosqueirnosv2: `/tenemosqueirnosv2.ogg`,
+    yaestabienporfavor: `/yaestabienporfavor.ogg`,
+
   }
 
   return (
@@ -15,6 +41,27 @@ function App() {
       <h1>Sinior Sénior película (dios)</h1>
       <SoundButton label="Rapido senior" fileUrl={audioList.rapidosenior} />
       <SoundButton label="Tenemos que irnos" fileUrl={audioList.tenemosqueirnos} />
+      <SoundButton label="Al campo y..." fileUrl={audioList.alcamnpoyacorrer} />
+      <SoundButton label="Corred insensatos!" fileUrl={audioList.corredinsensatos} />
+      <SoundButton label="Dios" fileUrl={audioList.dios} />
+      <SoundButton label="Dios x 3" fileUrl={audioList.diosdiosdios} />
+      <SoundButton label="El tio de los melindros" fileUrl={audioList.eltiodelosmelindros} />
+      <SoundButton label="Hay o no hay" fileUrl={audioList.hayonohay} />
+      <SoundButton label="La pulicia" fileUrl={audioList.lapulicia} />
+      <SoundButton label="Nos roban las patatas" fileUrl={audioList.nosrobanlaspatatas} />
+      <SoundButton label="No tocar los cojones" fileUrl={audioList.notocarloscojones} />
+      <SoundButton label="Película grande" fileUrl={audioList.peliculagrande} />
+      <SoundButton label="Película grande o pequeña" fileUrl={audioList.peliculagrandeopequena} />
+      <SoundButton label="Por favor, señor" fileUrl={audioList.porfavorsinior} />
+      <SoundButton label="Por favor, soy muy pobre" fileUrl={audioList.porfavorsoymuypobre} />
+      <SoundButton label="Prohibido tocar los cojones" fileUrl={audioList.prohibidotocarloscojones} />
+      <SoundButton label="Pulicia" fileUrl={audioList.pulicia} />
+      <SoundButton label="Rápido, por favor" fileUrl={audioList.rapidoporfavor} />
+      <SoundButton label="Rápido, por favor 2" fileUrl={audioList.rapidoporfavorv2} />
+      <SoundButton label="Señor" fileUrl={audioList.senioor} />
+      <SoundButton label="Señor es pobre" fileUrl={audioList.seniorespobre} />
+      <SoundButton label="Señor es pobre 2" fileUrl={audioList.seniorespobrev2} />
+      <SoundButton label="Sinior senior pelicula" fileUrl={audioList.siniorseniorpelicula} />
     </>
   )
 }
